6d5df476fb972d5c37e3eddf82fbba79c547d370
Rodolfo Martins <bonquers@gmai.com>
2026-02-14 19:20:23 -0300

    Add Variaveis para região AWS  e nome do projeto

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "Região da AWS"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Nome do projeto"
+  type        = string
+  default     = "sre-observability-bootstrap"
+}
