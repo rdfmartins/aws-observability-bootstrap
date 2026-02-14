c1423cf76ffda29f699dc6b617fd1dc5495afca7
Rodolfo Martins <bonquers@gmai.com>
2026-02-14 19:23:06 -0300

    Add IAM role e policies para  EC2 e SS e CloudWatch

---
+++ terraform/iam.tf
@@ -0,0 +1,36 @@
+# IAM Role para permitir que a EC2 se comunique com SSM e CloudWatch
+resource "aws_iam_role" "ec2_role" {
+  name = "${var.project_name}-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+# Anexando a política necessária para o AWS Systems Manager (SSM)
+resource "aws_iam_role_policy_attachment" "ssm_managed" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Anexando a política necessária para o CloudWatch Agent enviar métricas
+resource "aws_iam_role_policy_attachment" "cw_agent" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+# Instance Profile que será associado à instância EC2
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.project_name}-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
